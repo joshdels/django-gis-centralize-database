eacf69c381a2fd413e8ecbb45bf52dfb5c83f10a
joshdels <deleonjoshy2@gmail.com>
2026-01-24 17:38:56 +0800

    feat: project username serializer api

--- api/serializers.py
+++ api/serializers.py
@@ -4,27 +4,47 @@ from django.contrib.auth.models import User
 from accounts.models import Profile
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ["image", "first_name", "last_name", "location"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Used to serve the user's detail as an api"""
+
+    profile = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "profile"]
+
+
+class ProjectUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username"]
+
+
 class ProjectSerializer(serializers.ModelSerializer):
     """Project's served in the api"""
+
+    user = ProjectUserSerializer(read_only=True)
+
     class Meta:
         model = Project
-        fields = ["id", "user", "name", "description", "created_at", "updated_at", "is_private"]
+        fields = [
+            "id",
+            "user",
+            "name",
+            "description",
+            "created_at",
+            "updated_at",
+            "is_private",
+        ]
         read_only_fields = ["id", "created_at"]
 
     def create(self, validated_data):
         request = self.context["request"]
         validated_data["user"] = request.user
         return super().create(validated_data)
-    
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['image', 'first_name', 'last_name', 'location']
-
-class UserSerializer(serializers.ModelSerializer):
-    """Used to serve the user's detail as an api"""
-    profile = ProfileSerializer(read_only=True)
-    
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'profile']
