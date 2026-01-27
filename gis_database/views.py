54849dfc0a1c69d9a03dc345a740aa22a185b783
joshdels <assistantgisjosh@gmail.com>
2026-01-27 16:41:04 +0800

    feat: file versioning

--- gis_database/views.py
+++ gis_database/views.py
@@ -143,9 +143,16 @@ def download_project(request, pk):
     with zipfile.ZipFile(zip_buffer, "w") as zip_file:
         for pf in files:
             if pf.file:
-                filename = os.path.basename(pf.file.name)
+                stored_name = os.path.basename(pf.file.name)
+                base_name, ext = os.path.splitext(stored_name)
+
+                if "_v" in base_name:
+                    base_name = base_name.rsplit("_v", 1)[0]
+
+                download_name = f"{base_name}{ext}"
+
                 with pf.file.open("rb") as f:
-                    zip_file.writestr(filename, f.read())
+                    zip_file.writestr(download_name, f.read())
 
     zip_buffer.seek(0)
     response = HttpResponse(zip_buffer, content_type="application/zip")
@@ -153,56 +160,94 @@ def download_project(request, pk):
     return response
 
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.http import HttpResponse, Http404
+import os
+
+from .models import Project, File, FileActivity
+from .forms import ProjectForm
+from .utils import compute_hash
+from .views import unset_latest
+
+
 @login_required
 def update_file(request, pk):
     project = get_object_or_404(Project, pk=pk, owner=request.user)
-
-    project_file = project.files.filter(is_latest=True).order_by("-version").first()
-
-    if not project_file:
-        raise Http404("No latest file to update")
+    latest_file = project.files.filter(is_latest=True).order_by("-version").first()
 
     if request.method == "POST":
-        uploaded_file = request.FILES.get("uploaded_file")
-        if not uploaded_file:
-            raise Http404("No file uploaded")
+        form = ProjectForm(request.POST, request.FILES, owner=request.user)
 
-        profile = request.user.profile
-        if not profile.can_store(uploaded_file.size):
-            return HttpResponse("Storage quota exceeded", status=400)
+        if form.is_valid():
+            uploaded_file = form.cleaned_data["uploaded_file"]
+            profile = request.user.profile
 
-        new_hash = compute_hash(uploaded_file)
+            if not profile.can_store(uploaded_file.size):
+                # Future modal: storage quota exceeded
+                return HttpResponse("Storage quota exceeded", status=400)
 
-        if new_hash == project_file.hash:
-            return HttpResponse("No changes detected", status=400)
+            new_hash = compute_hash(uploaded_file)
 
-        with transaction.atomic():
-            unset_latest(request.user, project, project_file.name)
+            with transaction.atomic():
+                # Case 1: File with same hash exists -> mark as latest
+                existing_file_with_hash = project.files.filter(hash=new_hash).first()
+                if existing_file_with_hash:
+                    unset_latest(request.user, project, existing_file_with_hash.name)
+                    existing_file_with_hash.is_latest = True
+                    existing_file_with_hash.save(update_fields=["is_latest"])
+                    return redirect("file:project-sync", pk=project.id)
 
-            new_file = File.objects.create(
-                project=project,
-                owner=request.user,
-                name=project_file.name,
-                file_folder=project_file.file_folder,
-                file=uploaded_file,
-                hash=new_hash,
-                version=project_file.version + 1,
-                is_latest=True,
-            )
+                # Case 2: Same name, different hash -> version increment
+                latest_file_same_name = (
+                    project.files.filter(name=uploaded_file.name)
+                    .order_by("-version")
+                    .first()
+                )
+                if latest_file_same_name:
+                    unset_latest(request.user, project, latest_file_same_name.name)
+                    version = latest_file_same_name.version + 1
+                    file_folder = latest_file_same_name.file_folder
+                    action = "upload_new_version"
+                else:
+                    # Completely new file
+                    version = 1
+                    file_folder = os.path.splitext(uploaded_file.name)[0].replace(
+                        " ", "_"
+                    )
+                    action = "upload_new_file"
 
-            FileActivity.objects.create(
-                file=new_file,
-                owner=request.user,
-                action="upload_new_version",
-            )
+                # Create new File
+                new_file = File.objects.create(
+                    project=project,
+                    owner=request.user,
+                    name=uploaded_file.name,
+                    file_folder=file_folder,
+                    file=uploaded_file,
+                    hash=new_hash,
+                    version=version,
+                    is_latest=True,
+                )
 
-        return redirect("file:project-sync", pk=project.id) 
+                # Log activity
+                FileActivity.objects.create(
+                    file=new_file,
+                    owner=request.user,
+                    action=action,
+                )
+
+            return redirect("file:project-sync", pk=project.id)
+
+    else:
+        form = ProjectForm(owner=request.user)
 
     return render(
         request,
         "pages/update_file.html",
         {
-            "project_file": project_file,
+            "form": form,
+            "project_file": latest_file,
             "project": project,
         },
     )
@@ -218,7 +263,6 @@ def delete_file(request, pk):
         project_file.delete()
         return redirect("file:project-detail", pk=project_file.project.pk)
 
-    # Temporary fix. will make the delete.html
     return render(request, "components/file/file_delete.html", {"file": project_file})
 
 
